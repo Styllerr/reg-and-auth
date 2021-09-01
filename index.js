a54ea95eae50fcb1ec475dc92ab7d61c75f143be
Yevhenii_Chukavov <cork@ukr.net>
2021-09-01 10:25:51 +0300

    add expres and some routers

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const router = require('./router/index')
+
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use('/api', router);
+
 const PORT = process.env.NODE_PORT || 9000;
+const DB_URL = process.env.MONGO_URL || 'http://localhost:27017';
 
 const start = async () => {
     try {
+        await mongoose.connect(
+            DB_URL,
+            {useNewUrlParser: true})
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
     } catch (error) {
         console.log(error)
